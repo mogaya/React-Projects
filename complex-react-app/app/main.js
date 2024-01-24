8786bca63c75c9118a9e66e255e2b795c9d94346
mogaya <mogayainnocent@gmail.com>
2024-01-24 12:03:25 +0300

    additional componenets

--- complex-react-app/app/main.js
+++ complex-react-app/app/main.js
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function exampleComponent() {
+//Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+
+function Main() {
   return (
-    <div>
-      <h1>This is our app</h1>
-      <p>The sky is blue</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#app"));
-root.render(<exampleComponent />);
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
